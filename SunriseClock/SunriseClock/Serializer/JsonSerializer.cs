7ae7003961bf2ff70f919f158e79f3dcac74bcd4
Fabian Hauser <fabian@fh2.ch>
2016-12-01 13:16:37 +0100

    Add custom JSON serializer

---
+++ SunriseClock/SunriseClock/Serializer/JsonSerializer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp.Serializers;
+using RestSharp.Deserializers;
+
+namespace SunriseClock.Serializer
+{
+    internal class JsonSerializer : ISerializer, IDeserializer
+    {
+        private readonly Newtonsoft.Json.JsonSerializer serializer;
+
+        public JsonSerializer(Newtonsoft.Json.JsonSerializer serializer)
+        {
+            this.serializer = serializer;
+        }
+
+        public string ContentType
+        {
+            get { return "application/json"; }
+            set {  } // per Interface.
+        }
+        public string DateFormat { get; set; }
+        public string Namespace { get; set; }
+        public string RootElement { get; set; }
+
+        public string Serialize(object obj)
+        {
+            using (var stringWriter = new StringWriter())
+            {
+                using (var jsonTextWriter = new JsonTextWriter(stringWriter))
+                {
+                    serializer.Serialize(jsonTextWriter, obj);
+
+                    return stringWriter.ToString();
+                }
+            }
+        }
+
+        public T Deserialize<T>(RestSharp.IRestResponse response)
+        {
+            var content = response.Content;
+            
+            using (var stringReader = new StringReader(content))
+            {
+                using (var jsonTextReader = new JsonTextReader(stringReader))
+                {
+                    return serializer.Deserialize<T>(jsonTextReader);
+                }
+            }
+        }
+
+        public static JsonSerializer Default => new JsonSerializer(new Newtonsoft.Json.JsonSerializer()
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+        });
+    }
+}
--- SunriseClock/SunriseClock/Service/ClockApi.cs
+++ SunriseClock/SunriseClock/Service/ClockApi.cs
@@ -16,6 +16,14 @@ namespace SunriseClock.Service
         {
             var client = new RestClient();
             client.BaseUrl = BaseUrl;
+
+            // Override with Newtonsoft JSON Handler
+            client.AddHandler("application/json", Serializer.JsonSerializer.Default);
+            client.AddHandler("text/json", Serializer.JsonSerializer.Default);
+            client.AddHandler("text/x-json", Serializer.JsonSerializer.Default);
+            client.AddHandler("text/javascript", Serializer.JsonSerializer.Default);
+            client.AddHandler("*+json", Serializer.JsonSerializer.Default);
+
             var response = client.Execute<T>(request);
 
             if(response.ErrorException != null)
@@ -27,30 +35,17 @@ namespace SunriseClock.Service
 
         public Configuration GetConfiguration()
         {
-            //  var request = new RestRequest();
-            //  request.Resource = "configuration";
-            //   return Execute<Configuration>(request);
-            Configuration config = new Configuration();
-            config.Alarms = new List<Alarm>();
-
-            Alarm alarm = new Alarm();
-            alarm.Name = "Test";
-            alarm.Enabled = true;
-
-            Alarm alarm2 = new Alarm();
-            alarm2.Name = "Test 2";
-            alarm2.Enabled = true;
-
-            config.Alarms.Add(alarm);
-            config.Alarms.Add(alarm2);
-
-            return config;
+            var request = new RestRequest { Resource = "configuration" };
+            return Execute<Configuration>(request);
         }
 
         public void SetConfiguration(Configuration config)
         {
-            var request = new RestRequest(Method.POST);
-            request.Resource = "configuration";
+            var request = new RestRequest(Method.POST)
+            {
+                Resource = "configuration",
+                JsonSerializer = Serializer.JsonSerializer.Default
+            };
             request.AddJsonBody(config);
             var response = Execute<Configuration>(request);
         }
