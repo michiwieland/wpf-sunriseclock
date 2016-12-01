abfc1ba519402dcd8e362bcc65713bd67302df84
Michael Wieland <michi.wieland@hotmail.com>
2016-12-01 13:05:17 +0100

    Host Model added

---
+++ SunriseClock/SunriseClock/Model/Host.cs
@@ -0,0 +1,18 @@
+﻿using PropertyChanged;
+using RestSharp.Deserializers;
+using RestSharp.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunriseClock.Model
+{
+    [ImplementPropertyChanged]
+    class Host
+    {
+        public string Name { get; set; }
+        public int Port { get; set; }
+    }
+}
